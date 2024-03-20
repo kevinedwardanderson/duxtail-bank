7f6c6c116eacfc1bb004ae4782b74b45459c9d98
Kevin Anderson <kevinanderson@Kevins-MacBook-Pro.local>
2024-03-19 21:41:47 -0400

    Fix logo, icon, dashboard filter

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,4 +1,4 @@
-import BalanceBox from '../ui-components/SummaryBox';
+import SummaryBox from '../ui-components/SummaryBox';
 import ChartFilter from '../features/dashboard/ChartTransactionFilter';
 import AccountSelector from '../ui-components/AccountSelector';
 import Timeframe from '../features/dashboard/Timeframe';
@@ -14,13 +14,13 @@ function Dashboard() {
       <div className="relative row-start-1 row-end-8 flex flex-col justify-between ">
         <Chart />
 
-        <div className="absolute left-4 top-20 min-w-44">
+        <div className="absolute left-4 top-20 z-10 min-w-44">
           <AccountSelector
             feature={'dashboard'}
             featureAcct={dashboardAccount}
           />
         </div>
-        <BalanceBox />
+        <SummaryBox />
         <div className="absolute bottom-0 w-full  bg-slate-900 py-1">
           <ChartFilter />
         </div>
